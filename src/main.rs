802e69f8e7c85fd7d02d3f7bbb9b15fd733c5c9d
Puneet Dhiman <puneet.dhiman@synlabs.io>
2023-01-06 18:15:54 +0530

    Test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use clap::Parser;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-   /// Name of the person to greet
-   #[arg(short, long)]
-   name: String,
-
-   /// Number of times to greet
-   #[arg(short, long, default_value_t = 1)]
-   count: u8,
-}
+use std::io;
 
 fn main() {
-   let args = Args::parse();
+    let options = vec![
+        "View user details",
+        "List repositories",
+        "Clone repositories",
+    ];
 
-   for _ in 0..args.count {
-       println!("Hello {}!", args.name)
-   }
+    println!("Hello, which action do you want to perform?");
+
+    for (index, option) in options.iter().enumerate() {
+        println!("{}. {}", index + 1, option)
+    }
+
+    let mut choice = String::new();
+    let mut valid_choice: usize;
+
+    while true {
+        choice = "".to_string();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Error reading input");
+
+        if choice < options.len().to_string() {
+            valid_choice = choice.parse().unwrap();
+            println!("You have chosed {}", choice);
+
+            break;
+        }
+        println!("Invalid choice, enter again");
+    }
 }
